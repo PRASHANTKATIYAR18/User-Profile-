1c264d8e63f5ec8a2e195a0e8100ebb583432b36
PRASHANTKATIYAR18 <prashantkatiyar10796@gmil.com>
2019-10-18 23:43:18 +0530

    first commit upload this code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React ,{Component}  from 'react';
+import {} from "shards-react";
+import './sass/App.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import Routing from "./routing";
+import Mainavbar from './navbar';
+class App extends Component { 
+render(){
+  return(
+      <div>
+        <Mainavbar/>
+         <Routing/>
+        
+        </div>
+      );
+    }
+ }
 export default App;
+ 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
